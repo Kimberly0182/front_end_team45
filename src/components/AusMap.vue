23d1f4e1002fefcdeb7b1fb3db91a28864d0ff33
juicyka <qzzhu1@student.unimelb.edu.au>
2023-05-18 22:28:01 +1000

    Delete unused code

--- src/components/AusMap.vue
+++ src/components/AusMap.vue
@@ -1,11 +1,10 @@
 <template>
-  <div ref="mapDiv" style="width: 100%; height: 92.1vh" />
+  <div ref="ausMap" style="width: 100%; height: 92.1vh" />
 </template>
 
 <script>
 /* eslint-disable no-undef */
-import { computed, ref, onMounted } from "vue";
-import { useGeolocation } from "../useGeolocation";
+import { ref, onMounted } from "vue";
 import { Loader } from "@googlemaps/js-api-loader";
 
 const GOOGLE_MAPS_API_KEY = "AIzaSyDlJBBR9Jpp_kfM4ztufQUNan15FHYZzaw";
@@ -13,25 +12,19 @@ const GOOGLE_MAPS_API_KEY = "AIzaSyDlJBBR9Jpp_kfM4ztufQUNan15FHYZzaw";
 export default {
   name: "AusMap",
   setup() {
-    const { coords } = useGeolocation();
-    const currPos = computed(() => ({
-      lat: coords.value.latitude,
-      lng: coords.value.longitude,
-    }));
-
     const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY });
-    const mapDiv = ref(null);
+    const ausMap = ref(null);
     let map = ref(null);
 
     onMounted(async () => {
       await loader.load();
-      map.value = new google.maps.Map(mapDiv.value, {
-        center: currPos.value,
+      map.value = new google.maps.Map(ausMap.value, {
+        center: { lat: -28.5, lng: 135 },
         zoom: 4.6,
       });
     });
 
-    return { currPos, mapDiv };
+    return { ausMap };
   },
 };
 </script>
