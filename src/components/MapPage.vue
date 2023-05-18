1d53697d2520d9b58590958a4f2cace4653cb2ff
juicyka <qzzhu1@student.unimelb.edu.au>
2023-05-18 18:04:12 +1000

    Initial trial of Map (No map show)

--- src/components/MapPage.vue
+++ src/components/MapPage.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>
-      <h1>Welcome to the Map Page</h1>
-    </div>
-  </template>
-  
-  <script>
-  export default{
-    name: "MapPage"
-  };
-  </script>
+  <div>
+    <h1>Welcome to the Map Page</h1>
+    <AusMap />
+  </div>
+</template>
+
+<script>
+import AusMap from "./AusMap.vue";
+export default {
+  name: "MapPage",
+  components: {
+    AusMap,
+  },
+};
+</script>
