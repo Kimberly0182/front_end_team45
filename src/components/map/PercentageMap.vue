8e6918513b988d7ce3c59bbf11d70fc5d3323d19
juicyka <qzzhu1@student.unimelb.edu.au>
2023-05-19 15:02:40 +1000

    PercentageMap use id

--- src/components/map/PercentageMap.vue
+++ src/components/map/PercentageMap.vue
@@ -1,10 +1,10 @@
 <template>
-  <div ref="percentageMap" style="width: 100%; height: 92.1vh" />
+  <div id="percentageMap" style="width: 100%; height: 92.1vh" />
 </template>
 
 <script>
 /* eslint-disable no-undef */
-import { ref, onMounted } from "vue";
+import { onMounted } from "vue";
 import { Loader } from "@googlemaps/js-api-loader";
 
 const GOOGLE_MAPS_API_KEY = "AIzaSyDlJBBR9Jpp_kfM4ztufQUNan15FHYZzaw";
@@ -13,15 +13,15 @@ export default {
   name: "PercentageMap",
   setup() {
     const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY });
-    const percentageMap = ref(null);
-    let map = ref(null);
 
     onMounted(async () => {
       await loader.load();
-      map.value = new google.maps.Map(percentageMap.value, {
+
+      let map = new google.maps.Map(document.getElementById("percentageMap"), {
         center: { lat: -28.5, lng: 135 },
         zoom: 4.6,
       });
+      map.data.loadGeoJson("../../utils/map.json");
     });
 
     return { percentageMap };
